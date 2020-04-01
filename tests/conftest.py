94020faf8257c08cf7b9e6f67c37b71c99b5b267
Kamlesh Prajapat <41115735+Sem31@users.noreply.github.com>
2020-04-01 14:05:37 +0530

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,8 +7,8 @@ import sys
 
 @pytest.fixture
 def browser(request):
-    sauce_username = username
-    sauce_access_key = access-key
+    sauce_username = "kamlesh31"
+    sauce_access_key = "9320d3d8-2b86-468b-ab21-01e9ff48a3f8"
     remote_url = "https://ondemand.saucelabs.com:443/wd/hub"
 
     sauceOptions = {
