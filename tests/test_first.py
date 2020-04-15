781d7b4207db614a3d6841e86a512c6d74c1b1e6
Kamlesh Prajapat <41115735+Sem31@users.noreply.github.com>
2020-04-15 12:26:21 +0530

    Update test_first.py

--- tests/test_first.py
+++ tests/test_first.py
@@ -15,9 +15,9 @@ class TestCase(BaseTest):
     search_page.search(PHRASE)
     # Verify that results appear
     result_page = DuckDuckGoResultPage(browser)
-    assert result_page.link_div_count() > 1
+    assert result_page.link_div_count() > 0
     assert result_page.phrase_result_count(PHRASE) > 0
-    assert result_page.search_input_value() == PHRASE
+    assert result_page.search_input_value() == PHRASE0
 
   # def test_basic_duckduckgo_search_1(self,browser):
   #   # Set up test case data
